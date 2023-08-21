e9a84834494e577c8683ff5e394f03f9b4888eed
Dan Gowans <dangowans@users.noreply.github.com>
2023-08-21 10:38:39 -0400

    table output on dashboard

--- public-typescript/dashboard.js
+++ public-typescript/dashboard.js
@@ -1,6 +1,6 @@
 "use strict";
 // eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable unicorn/prefer-module */
+/* eslint-disable @typescript-eslint/indent */
 Object.defineProperty(exports, "__esModule", { value: true });
 (() => {
     var _a, _b;
@@ -9,16 +9,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
     const dashboardFormElement = document.querySelector('#form--dashboard');
     let charts = {};
     function getChartKey(assetId, dataTypeId) {
-        return `${assetId}::${dataTypeId}`;
+        return `${assetId}_${dataTypeId}`;
     }
     function formatDateLabel(timeSeconds) {
         return new Date(timeSeconds * 1000).toLocaleString();
     }
     function addDataToChart(chart, label, newData) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         chart.data.labels.push(label);
         for (const dataset of chart.data.datasets) {
             dataset.data.push(newData);
         }
+    }
+    function addDataToTable(table, data) {
+        var _a, _b, _c, _d;
+        const rowElement = document.createElement('tr');
+        rowElement.innerHTML = `<td>${formatDateLabel(data.timeSeconds)}</td>
+      <td>${(_a = data.readingType) !== null && _a !== void 0 ? _a : ''}</td>
+      <td>${(_b = data.commodity) !== null && _b !== void 0 ? _b : ''}</td>
+      <td class="has-text-right">${data.dataValue}</td>
+      <td>${(_c = data.unit) !== null && _c !== void 0 ? _c : ''}</td>`;
+        (_d = table.querySelector('tbody')) === null || _d === void 0 ? void 0 : _d.append(rowElement);
     }
     function getEnergyData() {
         dashboardContainer.innerHTML = `<p class="has-text-centered has-text-grey">
@@ -33,7 +44,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
             for (const dataItem of responseJSON.energyData) {
                 const chartKey = getChartKey(dataItem.assetId, dataItem.dataTypeId);
                 if (charts[chartKey] === undefined) {
-                    console.log(dataItem);
                     const panelElement = document.createElement('div');
                     panelElement.className = 'panel';
                     panelElement.innerHTML = `<div class="panel-heading">
@@ -42,19 +52,51 @@ Object.defineProperty(exports, "__esModule", { value: true });
                   <div class="level-item">
                     <i class="${(_a = dataItem.fontAwesomeIconClasses) !== null && _a !== void 0 ? _a : 'fas fa-bolt'}" aria-hidden="true"></i>
                   </div>
-                  <div class="level-item">
+                  <div class="level-item is-block">
                     <h2 class="has-text-weight-bold" data-field="assetName"></h2>
+                    <span class="has-text-weight-normal" data-field="serviceCategory"></span>
                   </div>
                 </div>
                 <div class="level-right">
                   <div class="level-item">
-                    <span class="has-text-weight-normal" data-field="serviceCategory"></span>
+                    <div class="tabs is-toggle is-toggle-rounded">
+                      <ul>
+                        <li class="is-active">
+                          <a href="#chartTab_${chartKey}">
+                            <span class="icon is-small"><i class="fas fa-chart-bar" aria-hidden="true"></i></span>
+                            <span>Chart</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#tableTab_${chartKey}">
+                            <span class="icon is-small"><i class="fas fa-table" aria-hidden="true"></i></span>
+                            <span>Table</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
               </div>
-              <div class="panel-block is-block">
-              <canvas style="max-height:400px"></canvas>
+              <div class="tabs-container panel-block is-block">
+                <div id="chartTab_${chartKey}">
+                  <canvas style="max-height:400px"></canvas>
+                </div>
+                <div id="tableTab_${chartKey}" class="is-hidden">
+                  <table class="table is-fullwidth is-striped is-hoverable has-sticky-header">
+                    <thead>
+                      <tr>
+                        <th>Time</th>
+                        <th>Reading Type</th>
+                        <th>Commodity</th>
+                        <th class="has-text-right">Value</th>
+                        <th>Unit</th>
+                      </tr>
+                    </thead>
+                    <tbody></tbody>
+                  </table>
+                </div>
               </div>`;
                     panelElement.querySelector('[data-field="assetName"]').textContent = (_b = dataItem.assetName) !== null && _b !== void 0 ? _b : '';
                     panelElement.querySelector('[data-field="serviceCategory"]').textContent = (_c = dataItem.serviceCategory) !== null && _c !== void 0 ? _c : '';
@@ -74,15 +116,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
                             ]
                         }
                     });
-                    charts[chartKey] = chart;
+                    const table = panelElement.querySelector('table');
+                    charts[chartKey] = {
+                        chart,
+                        table
+                    };
+                    addDataToTable(table, dataItem);
                 }
                 else {
-                    addDataToChart(charts[chartKey], formatDateLabel(dataItem.timeSeconds), dataItem.dataValue * Math.pow(10, dataItem.powerOfTenMultiplier));
+                    addDataToChart(charts[chartKey].chart, formatDateLabel(dataItem.timeSeconds), dataItem.dataValue * Math.pow(10, dataItem.powerOfTenMultiplier));
+                    addDataToTable(charts[chartKey].table, dataItem);
                 }
             }
             for (const chart of Object.values(charts)) {
-                chart.update();
+                chart.chart.update();
             }
+            bulmaJS.init(dashboardContainer);
         });
     }
     /*
--- public-typescript/dashboard.ts
+++ public-typescript/dashboard.ts
@@ -1,4 +1,7 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable @typescript-eslint/indent */
+
+// eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable unicorn/prefer-module */
 
 // eslint-disable-next-line n/no-missing-import
@@ -24,22 +27,41 @@ declare const cityssm: cityssmGlobal
     '#form--dashboard'
   ) as HTMLFormElement
 
-  let charts: Record<string, Chart> = {}
+  let charts: Record<
+    string,
+    {
+      chart: Chart
+      table: HTMLTableElement
+    }
+  > = {}
 
   function getChartKey(assetId: number, dataTypeId: number): string {
-    return `${assetId}::${dataTypeId}`
+    return `${assetId}_${dataTypeId}`
   }
 
   function formatDateLabel(timeSeconds: number): string {
     return new Date(timeSeconds * 1000).toLocaleString()
   }
 
-  function addDataToChart(chart: Chart, label: any, newData: number): void {
+  function addDataToChart(chart: Chart, label: unknown, newData: number): void {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     chart.data.labels!.push(label)
 
     for (const dataset of chart.data.datasets) {
       dataset.data.push(newData)
     }
+  }
+
+  function addDataToTable(table: HTMLTableElement, data: EnergyData): void {
+    const rowElement = document.createElement('tr')
+
+    rowElement.innerHTML = `<td>${formatDateLabel(data.timeSeconds)}</td>
+      <td>${data.readingType ?? ''}</td>
+      <td>${data.commodity ?? ''}</td>
+      <td class="has-text-right">${data.dataValue}</td>
+      <td>${data.unit ?? ''}</td>`
+
+    table.querySelector('tbody')?.append(rowElement)
   }
 
   function getEnergyData(): void {
@@ -66,8 +88,6 @@ declare const cityssm: cityssmGlobal
           )
 
           if (charts[chartKey] === undefined) {
-            console.log(dataItem)
-
             const panelElement = document.createElement('div')
             panelElement.className = 'panel'
 
@@ -79,19 +99,51 @@ declare const cityssm: cityssmGlobal
                       dataItem.fontAwesomeIconClasses ?? 'fas fa-bolt'
                     }" aria-hidden="true"></i>
                   </div>
-                  <div class="level-item">
+                  <div class="level-item is-block">
                     <h2 class="has-text-weight-bold" data-field="assetName"></h2>
+                    <span class="has-text-weight-normal" data-field="serviceCategory"></span>
                   </div>
                 </div>
                 <div class="level-right">
                   <div class="level-item">
-                    <span class="has-text-weight-normal" data-field="serviceCategory"></span>
+                    <div class="tabs is-toggle is-toggle-rounded">
+                      <ul>
+                        <li class="is-active">
+                          <a href="#chartTab_${chartKey}">
+                            <span class="icon is-small"><i class="fas fa-chart-bar" aria-hidden="true"></i></span>
+                            <span>Chart</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#tableTab_${chartKey}">
+                            <span class="icon is-small"><i class="fas fa-table" aria-hidden="true"></i></span>
+                            <span>Table</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
               </div>
-              <div class="panel-block is-block">
-              <canvas style="max-height:400px"></canvas>
+              <div class="tabs-container panel-block is-block">
+                <div id="chartTab_${chartKey}">
+                  <canvas style="max-height:400px"></canvas>
+                </div>
+                <div id="tableTab_${chartKey}" class="is-hidden">
+                  <table class="table is-fullwidth is-striped is-hoverable has-sticky-header">
+                    <thead>
+                      <tr>
+                        <th>Time</th>
+                        <th>Reading Type</th>
+                        <th>Commodity</th>
+                        <th class="has-text-right">Value</th>
+                        <th>Unit</th>
+                      </tr>
+                    </thead>
+                    <tbody></tbody>
+                  </table>
+                </div>
               </div>`
             ;(
               panelElement.querySelector(
@@ -125,19 +177,32 @@ declare const cityssm: cityssmGlobal
               }
             )
 
-            charts[chartKey] = chart
+            const table = panelElement.querySelector(
+              'table'
+            ) as HTMLTableElement
+
+            charts[chartKey] = {
+              chart,
+              table
+            }
+
+            addDataToTable(table, dataItem)
           } else {
             addDataToChart(
-              charts[chartKey],
+              charts[chartKey].chart,
               formatDateLabel(dataItem.timeSeconds),
               dataItem.dataValue * Math.pow(10, dataItem.powerOfTenMultiplier)
             )
+
+            addDataToTable(charts[chartKey].table, dataItem)
           }
         }
 
         for (const chart of Object.values(charts)) {
-          chart.update()
+          chart.chart.update()
         }
+
+        bulmaJS.init(dashboardContainer)
       }
     )
   }
