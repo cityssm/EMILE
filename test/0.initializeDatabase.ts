1de4a291312371f9c2ce305f76082bb0c978251c
Dan Gowans <dangowans@users.noreply.github.com>
2023-08-23 13:46:04 -0400

    add initializeDatabase

---
+++ test/0.initializeDatabase.ts
@@ -0,0 +1,22 @@
+import * as assert from 'node:assert'
+import fs from 'node:fs/promises'
+
+import { initializeDatabase } from '../database/initializeDatabase.js'
+import {
+  databasePath,
+  useTestDatabases
+} from '../helpers/functions.database.js'
+
+describe('Initialize Database', () => {
+  it('initializes a cemetery database', async () => {
+    if (!useTestDatabases) {
+      assert.fail('Test database must be used!')
+    }
+
+    await fs.unlink(databasePath)
+
+    initializeDatabase()
+
+    assert.ok(true)
+  })
+})
