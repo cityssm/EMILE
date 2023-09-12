f0b83f2d8b2b775b3f189052b7b1c62b7f3c83b5
Dan Gowans <dangowans@users.noreply.github.com>
2023-09-12 13:00:03 -0400

    attempt to reduce complexity

--- helpers/functions.greenButton.js
+++ helpers/functions.greenButton.js
@@ -16,6 +16,64 @@ const greenButtonUser = {
     canUpdate: true,
     isAdmin: false
 };
+function getAssetIdFromIntervalBlock(intervalBlockEntry) {
+    let assetId;
+    let assetAlias = intervalBlockEntry.links.self ?? '';
+    if (assetAlias === undefined) {
+        throw new Error('No asset alias available on IntervalBlock entry.');
+    }
+    if (assetAlias.includes('/MeterReading/')) {
+        assetAlias = assetAlias.slice(0, Math.max(0, assetAlias.indexOf('/MeterReading/')));
+    }
+    console.log(`assetAlias: ${assetAlias}`);
+    const asset = getAssetByAssetAlias(assetAlias, greenButtonAssetAliasType?.aliasTypeId);
+    if (asset === undefined) {
+        const assetCategory = getAssetCategories()[0];
+        if (assetCategory === undefined) {
+            throw new Error(`Cannot create asset ${assetAlias} with no asset categories available.`);
+        }
+        assetId = addAsset({
+            assetName: assetAlias,
+            categoryId: assetCategory.categoryId
+        }, greenButtonUser);
+        addAssetAlias({
+            assetId,
+            aliasTypeId: greenButtonAssetAliasType?.aliasTypeId,
+            assetAlias
+        }, greenButtonUser);
+    }
+    else {
+        assetId = asset.assetId;
+    }
+    return assetId;
+}
+function getEnergyDataTypeAndPowerOfTenMultiplier(greenButtonJson, intervalBlockEntry) {
+    const meterReadingEntry = greenButtonHelpers.getMeterReadingEntryFromIntervalBlockEntry(greenButtonJson, intervalBlockEntry);
+    if (meterReadingEntry === undefined) {
+        throw new Error('Unable to find related MeterReading entry.');
+    }
+    const readingType = greenButtonHelpers.getReadingTypeEntryFromMeterReadingEntry(greenButtonJson, meterReadingEntry);
+    if (readingType === undefined) {
+        throw new Error('Unable to find related ReadingType entry.');
+    }
+    const usagePoint = greenButtonHelpers.getUsagePointEntryFromMeterReadingEntry(greenButtonJson, meterReadingEntry);
+    if (usagePoint === undefined) {
+        throw new Error('Unable to find related UsagePoint entry.');
+    }
+    const powerOfTenMultiplier = readingType.content.ReadingType.powerOfTenMultiplier ?? '0';
+    return {
+        energyDataType: getEnergyDataTypeByGreenButtonIds({
+            serviceCategoryId: usagePoint.content.UsagePoint.ServiceCategory?.kind.toString() ?? '',
+            unitId: readingType.content.ReadingType.uom?.toString() ?? '',
+            readingTypeId: readingType.content.ReadingType.kind?.toString(),
+            commodityId: readingType.content.ReadingType.commodity?.toString(),
+            accumulationBehaviourId: readingType.content.ReadingType.accumulationBehaviour?.toString()
+        }, greenButtonUser, true),
+        powerOfTenMultiplier: typeof powerOfTenMultiplier === 'string'
+            ? Number.parseInt(powerOfTenMultiplier, 10)
+            : powerOfTenMultiplier
+    };
+}
 export function recordGreenButtonData(greenButtonJson, options) {
     let recordCount = 0;
     const intervalBlockEntries = greenButtonHelpers.getEntriesByContentType(greenButtonJson, 'IntervalBlock');
@@ -25,54 +83,11 @@ export function recordGreenButtonData(greenButtonJson, options) {
     for (const intervalBlockEntry of intervalBlockEntries) {
         let assetId = options.assetId;
         if ((assetId ?? '') === '') {
-            let assetAlias = intervalBlockEntry.links.self ?? '';
-            if (assetAlias === undefined) {
-                throw new Error('No asset alias available on IntervalBlock entry.');
-            }
-            if (assetAlias.includes('/MeterReading/')) {
-                assetAlias = assetAlias.slice(0, Math.max(0, assetAlias.indexOf('/MeterReading/')));
-            }
-            console.log(`assetAlias: ${assetAlias}`);
-            const asset = getAssetByAssetAlias(assetAlias, greenButtonAssetAliasType?.aliasTypeId);
-            if (asset === undefined) {
-                const assetCategory = getAssetCategories()[0];
-                if (assetCategory === undefined) {
-                    throw new Error(`Cannot create asset ${assetAlias} with no asset categories available.`);
-                }
-                assetId = addAsset({
-                    assetName: assetAlias,
-                    categoryId: assetCategory.categoryId
-                }, greenButtonUser);
-                addAssetAlias({
-                    assetId,
-                    aliasTypeId: greenButtonAssetAliasType?.aliasTypeId,
-                    assetAlias
-                }, greenButtonUser);
-            }
-            else {
-                assetId = asset.assetId;
-            }
+            assetId = getAssetIdFromIntervalBlock(intervalBlockEntry);
         }
-        const meterReadingEntry = greenButtonHelpers.getMeterReadingEntryFromIntervalBlockEntry(greenButtonJson, intervalBlockEntry);
-        if (meterReadingEntry === undefined) {
-            throw new Error('Unable to find related MeterReading entry.');
-        }
-        const readingType = greenButtonHelpers.getReadingTypeEntryFromMeterReadingEntry(greenButtonJson, meterReadingEntry);
-        if (readingType === undefined) {
-            throw new Error('Unable to find related ReadingType entry.');
-        }
-        const usagePoint = greenButtonHelpers.getUsagePointEntryFromMeterReadingEntry(greenButtonJson, meterReadingEntry);
-        if (usagePoint === undefined) {
-            throw new Error('Unable to find related UsagePoint entry.');
-        }
-        const energyDataType = getEnergyDataTypeByGreenButtonIds({
-            serviceCategoryId: usagePoint.content.UsagePoint.ServiceCategory?.kind.toString() ?? '',
-            unitId: readingType.content.ReadingType.uom?.toString() ?? '',
-            readingTypeId: readingType.content.ReadingType.kind?.toString(),
-            commodityId: readingType.content.ReadingType.commodity?.toString(),
-            accumulationBehaviourId: readingType.content.ReadingType.accumulationBehaviour?.toString()
-        }, greenButtonUser, true);
-        if (energyDataType === undefined) {
+        const energyDataTypeAndPower = getEnergyDataTypeAndPowerOfTenMultiplier(greenButtonJson, intervalBlockEntry);
+        if (energyDataTypeAndPower === undefined ||
+            energyDataTypeAndPower.energyDataType === undefined) {
             throw new Error('Unable to retrieve EnergyDataType.');
         }
         for (const intervalBlock of intervalBlockEntry.content.IntervalBlock) {
@@ -81,34 +96,31 @@ export function recordGreenButtonData(greenButtonJson, options) {
                     intervalReading.value !== undefined) {
                     const currentDataPoint = getEnergyDataPoint({
                         assetId: assetId,
-                        dataTypeId: energyDataType.dataTypeId,
+                        dataTypeId: energyDataTypeAndPower.energyDataType
+                            .dataTypeId,
                         timeSeconds: intervalReading.timePeriod.start,
                         durationSeconds: intervalReading.timePeriod.duration
                     });
-                    const powerOfTenMultiplier = readingType.content.ReadingType.powerOfTenMultiplier ?? '0';
                     if (currentDataPoint === undefined) {
                         addEnergyData({
                             assetId,
-                            dataTypeId: energyDataType.dataTypeId,
+                            dataTypeId: energyDataTypeAndPower.energyDataType.dataTypeId,
                             fileId: options.fileId,
                             timeSeconds: intervalReading.timePeriod?.start,
                             durationSeconds: intervalReading.timePeriod?.duration,
                             dataValue: intervalReading.value,
-                            powerOfTenMultiplier: typeof powerOfTenMultiplier === 'string'
-                                ? Number.parseInt(powerOfTenMultiplier, 10)
-                                : powerOfTenMultiplier
+                            powerOfTenMultiplier: energyDataTypeAndPower.powerOfTenMultiplier
                         }, greenButtonUser);
                         recordCount += 1;
                     }
                     else if (currentDataPoint.dataValue !== intervalReading.value ||
-                        currentDataPoint.powerOfTenMultiplier !== powerOfTenMultiplier) {
+                        currentDataPoint.powerOfTenMultiplier !==
+                            energyDataTypeAndPower.powerOfTenMultiplier) {
                         updateEnergyDataValue({
                             dataId: currentDataPoint.dataId,
                             fileId: options.fileId,
                             dataValue: intervalReading.value,
-                            powerOfTenMultiplier: typeof powerOfTenMultiplier === 'string'
-                                ? Number.parseInt(powerOfTenMultiplier, 10)
-                                : powerOfTenMultiplier
+                            powerOfTenMultiplier: energyDataTypeAndPower.powerOfTenMultiplier
                         }, greenButtonUser);
                         recordCount += 1;
                     }
--- helpers/functions.greenButton.ts
+++ helpers/functions.greenButton.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable @typescript-eslint/indent */
+
 import {
   helpers as greenButtonHelpers,
   type types as GreenButtonTypes
@@ -11,6 +14,7 @@ import { getAssetAliasTypeByAliasTypeKey } from '../database/getAssetAliasType.j
 import { getEnergyDataPoint } from '../database/getEnergyData.js'
 import { getEnergyDataTypeByGreenButtonIds } from '../database/getEnergyDataType.js'
 import { updateEnergyDataValue } from '../database/updateEnergyData.js'
+import type { EnergyDataType } from '../types/recordTypes.js'
 
 import { getAssetCategories } from './functions.cache.js'
 
@@ -25,6 +29,126 @@ const greenButtonUser: EmileUser = {
   canLogin: true,
   canUpdate: true,
   isAdmin: false
+}
+
+function getAssetIdFromIntervalBlock(
+  intervalBlockEntry: GreenButtonTypes.GreenButtonEntryWithIntervalBlockContent
+): number {
+  let assetId: number
+
+  let assetAlias = intervalBlockEntry.links.self ?? ''
+
+  if (assetAlias === undefined) {
+    throw new Error('No asset alias available on IntervalBlock entry.')
+  }
+
+  if (assetAlias.includes('/MeterReading/')) {
+    assetAlias = assetAlias.slice(
+      0,
+      Math.max(0, assetAlias.indexOf('/MeterReading/'))
+    )
+  }
+
+  console.log(`assetAlias: ${assetAlias}`)
+
+  const asset = getAssetByAssetAlias(
+    assetAlias,
+    greenButtonAssetAliasType?.aliasTypeId
+  )
+
+  // Create asset
+  if (asset === undefined) {
+    const assetCategory = getAssetCategories()[0]
+
+    if (assetCategory === undefined) {
+      throw new Error(
+        `Cannot create asset ${assetAlias} with no asset categories available.`
+      )
+    }
+
+    assetId = addAsset(
+      {
+        assetName: assetAlias,
+        categoryId: assetCategory.categoryId
+      },
+      greenButtonUser
+    )
+
+    addAssetAlias(
+      {
+        assetId,
+        aliasTypeId: greenButtonAssetAliasType?.aliasTypeId,
+        assetAlias
+      },
+      greenButtonUser
+    )
+  } else {
+    assetId = asset.assetId as number
+  }
+
+  return assetId
+}
+
+function getEnergyDataTypeAndPowerOfTenMultiplier(
+  greenButtonJson: GreenButtonTypes.GreenButtonJson,
+  intervalBlockEntry: GreenButtonTypes.GreenButtonEntryWithIntervalBlockContent
+):
+  | undefined
+  | {
+      energyDataType?: EnergyDataType
+      powerOfTenMultiplier: number
+    } {
+  const meterReadingEntry =
+    greenButtonHelpers.getMeterReadingEntryFromIntervalBlockEntry(
+      greenButtonJson,
+      intervalBlockEntry
+    )
+
+  if (meterReadingEntry === undefined) {
+    throw new Error('Unable to find related MeterReading entry.')
+  }
+
+  const readingType =
+    greenButtonHelpers.getReadingTypeEntryFromMeterReadingEntry(
+      greenButtonJson,
+      meterReadingEntry
+    )
+
+  if (readingType === undefined) {
+    throw new Error('Unable to find related ReadingType entry.')
+  }
+
+  const usagePoint = greenButtonHelpers.getUsagePointEntryFromMeterReadingEntry(
+    greenButtonJson,
+    meterReadingEntry
+  )
+
+  if (usagePoint === undefined) {
+    throw new Error('Unable to find related UsagePoint entry.')
+  }
+
+  const powerOfTenMultiplier =
+    readingType.content.ReadingType.powerOfTenMultiplier ?? '0'
+
+  return {
+    energyDataType: getEnergyDataTypeByGreenButtonIds(
+      {
+        serviceCategoryId:
+          usagePoint.content.UsagePoint.ServiceCategory?.kind.toString() ?? '',
+        unitId: readingType.content.ReadingType.uom?.toString() ?? '',
+        readingTypeId: readingType.content.ReadingType.kind?.toString(),
+        commodityId: readingType.content.ReadingType.commodity?.toString(),
+        accumulationBehaviourId:
+          readingType.content.ReadingType.accumulationBehaviour?.toString()
+      },
+      greenButtonUser,
+      true
+    ),
+    powerOfTenMultiplier:
+      typeof powerOfTenMultiplier === 'string'
+        ? Number.parseInt(powerOfTenMultiplier, 10)
+        : powerOfTenMultiplier
+  }
 }
 
 export function recordGreenButtonData(
@@ -53,106 +177,22 @@ export function recordGreenButtonData(
     let assetId = options.assetId
 
     if ((assetId ?? '') === '') {
-      let assetAlias = intervalBlockEntry.links.self ?? ''
-
-      if (assetAlias === undefined) {
-        throw new Error('No asset alias available on IntervalBlock entry.')
-      }
-
-      if (assetAlias.includes('/MeterReading/')) {
-        assetAlias = assetAlias.slice(
-          0,
-          Math.max(0, assetAlias.indexOf('/MeterReading/'))
-        )
-      }
-
-      console.log(`assetAlias: ${assetAlias}`)
-
-      const asset = getAssetByAssetAlias(
-        assetAlias,
-        greenButtonAssetAliasType?.aliasTypeId
-      )
-
-      // Create asset
-      if (asset === undefined) {
-        const assetCategory = getAssetCategories()[0]
-
-        if (assetCategory === undefined) {
-          throw new Error(
-            `Cannot create asset ${assetAlias} with no asset categories available.`
-          )
-        }
-
-        assetId = addAsset(
-          {
-            assetName: assetAlias,
-            categoryId: assetCategory.categoryId
-          },
-          greenButtonUser
-        )
-
-        addAssetAlias(
-          {
-            assetId,
-            aliasTypeId: greenButtonAssetAliasType?.aliasTypeId,
-            assetAlias
-          },
-          greenButtonUser
-        )
-      } else {
-        assetId = asset.assetId as number
-      }
+      assetId = getAssetIdFromIntervalBlock(intervalBlockEntry)
     }
 
     /*
      * Ensure a dataTypeId is available
      */
 
-    const meterReadingEntry =
-      greenButtonHelpers.getMeterReadingEntryFromIntervalBlockEntry(
-        greenButtonJson,
-        intervalBlockEntry
-      )
-
-    if (meterReadingEntry === undefined) {
-      throw new Error('Unable to find related MeterReading entry.')
-    }
-
-    const readingType =
-      greenButtonHelpers.getReadingTypeEntryFromMeterReadingEntry(
-        greenButtonJson,
-        meterReadingEntry
-      )
-
-    if (readingType === undefined) {
-      throw new Error('Unable to find related ReadingType entry.')
-    }
-
-    const usagePoint =
-      greenButtonHelpers.getUsagePointEntryFromMeterReadingEntry(
-        greenButtonJson,
-        meterReadingEntry
-      )
-
-    if (usagePoint === undefined) {
-      throw new Error('Unable to find related UsagePoint entry.')
-    }
-
-    const energyDataType = getEnergyDataTypeByGreenButtonIds(
-      {
-        serviceCategoryId:
-          usagePoint.content.UsagePoint.ServiceCategory?.kind.toString() ?? '',
-        unitId: readingType.content.ReadingType.uom?.toString() ?? '',
-        readingTypeId: readingType.content.ReadingType.kind?.toString(),
-        commodityId: readingType.content.ReadingType.commodity?.toString(),
-        accumulationBehaviourId:
-          readingType.content.ReadingType.accumulationBehaviour?.toString()
-      },
-      greenButtonUser,
-      true
+    const energyDataTypeAndPower = getEnergyDataTypeAndPowerOfTenMultiplier(
+      greenButtonJson,
+      intervalBlockEntry
     )
 
-    if (energyDataType === undefined) {
+    if (
+      energyDataTypeAndPower === undefined ||
+      energyDataTypeAndPower.energyDataType === undefined
+    ) {
       throw new Error('Unable to retrieve EnergyDataType.')
     }
 
@@ -168,27 +208,23 @@ export function recordGreenButtonData(
         ) {
           const currentDataPoint = getEnergyDataPoint({
             assetId: assetId as number,
-            dataTypeId: energyDataType.dataTypeId as number,
+            dataTypeId: energyDataTypeAndPower.energyDataType
+              .dataTypeId as number,
             timeSeconds: intervalReading.timePeriod.start,
             durationSeconds: intervalReading.timePeriod.duration
           })
-
-          const powerOfTenMultiplier =
-            readingType.content.ReadingType.powerOfTenMultiplier ?? '0'
 
           if (currentDataPoint === undefined) {
             addEnergyData(
               {
                 assetId,
-                dataTypeId: energyDataType.dataTypeId,
+                dataTypeId: energyDataTypeAndPower.energyDataType.dataTypeId,
                 fileId: options.fileId,
                 timeSeconds: intervalReading.timePeriod?.start,
                 durationSeconds: intervalReading.timePeriod?.duration,
                 dataValue: intervalReading.value,
                 powerOfTenMultiplier:
-                  typeof powerOfTenMultiplier === 'string'
-                    ? Number.parseInt(powerOfTenMultiplier, 10)
-                    : powerOfTenMultiplier
+                  energyDataTypeAndPower.powerOfTenMultiplier
               },
               greenButtonUser
             )
@@ -196,7 +232,8 @@ export function recordGreenButtonData(
             recordCount += 1
           } else if (
             currentDataPoint.dataValue !== intervalReading.value ||
-            currentDataPoint.powerOfTenMultiplier !== powerOfTenMultiplier
+            currentDataPoint.powerOfTenMultiplier !==
+              energyDataTypeAndPower.powerOfTenMultiplier
           ) {
             updateEnergyDataValue(
               {
@@ -204,9 +241,7 @@ export function recordGreenButtonData(
                 fileId: options.fileId,
                 dataValue: intervalReading.value,
                 powerOfTenMultiplier:
-                  typeof powerOfTenMultiplier === 'string'
-                    ? Number.parseInt(powerOfTenMultiplier, 10)
-                    : powerOfTenMultiplier
+                  energyDataTypeAndPower.powerOfTenMultiplier
               },
               greenButtonUser
             )
